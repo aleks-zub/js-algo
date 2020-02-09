26a2125169db2a3fa06cef853ebc61351e3356c3
aleks-zub <60668996+aleks-zub@users.noreply.github.com>
2020-02-09 19:07:40 +0200

    Added algo for triangle

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 function writeAlgoDemo1() {
     var side_a = Math.random() * 10 + 5;
     var side_b = Math.random() * 10 + 5;
-    document.write("Side a=" + side_a + "Side b=" + side_b);
-    document.write("Объем куба" + side_a * side_a * side_a);
-    document.write("Плошадь квадрата" + side_a * side_a);
-    document.write("Плошадь прямоугольника" + side_a * side_b);
+    document.write("<p>Side a=" + side_a + "</p><p>Side b=" + side_b);
+    document.write("<p>Объем куба" + side_a * side_a * side_a + "</p>");
+    document.write("<p>Плошадь квадрата" + side_a * side_a + "</p>");
+    document.write("<p>Плошадь прямоугольника" + side_a * side_b + "</p>");
 }
 
 function writeAlgoDemo2() {
