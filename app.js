a7226d712fb5b6bd16c3aa8817ab9621d25dc2cf
aleks-zub <60668996+aleks-zub@users.noreply.github.com>
2020-02-09 18:51:18 +0200

    Update index.html
    Changes for next implementation of algorithms

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-function writeAlgoDemo() {
+function writeAlgoDemo1() {
     var side_a = Math.random() * 10 + 5;
     var side_b = Math.random() * 10 + 5;
     document.write("Side a=" + side_a + "Side b=" + side_b);
     document.write("Объем куба" + side_a * side_a * side_a);
     document.write("Плошадь квадрата" + side_a * side_a);
     document.write("Плошадь прямоугольника" + side_a * side_b);
+}
+
+function writeAlgoDemo2() {
+
 }
